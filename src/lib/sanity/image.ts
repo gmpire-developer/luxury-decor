606df94ff170c161c11c77cf4a5e49defd1e0a51
Gurvinder Singh <gurvindersinghmaan33@gmail.com>
2025-06-17 17:01:31 +0530

    Fix: resolve lintinig issues

--- src/lib/sanity/image.ts
+++ src/lib/sanity/image.ts
@@ -1,9 +1,10 @@
 // /lib/sanity.image.ts
 import imageUrlBuilder from '@sanity/image-url';
 import { client } from './client';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: any) {
+export function urlFor(source: SanityImageSource) {
   return builder.image(source);
 }
